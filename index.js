48e69c83ec960592456a32733244b144480d996a
ezeun <consistency1231@gmail.com>
2022-02-02 12:21:14 +0900

    #13 Auth 기능 만들기

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const express = require('express') //다운받은 express모듈을 가져옴
 const app = express() //함수를 이용해서 새로운 express앱을 만듦
 const port = 7000 //아무숫자 해도됨. 백엔드 서버
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
-
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-
-const {User} =require("./models/User");
+const { auth } = require('./middleware/auth');
+const { User } = require("./models/User");
 
 //bodyParser의 역할: 클라이언트에서 오는 정보를 서버에서 분석하여 가져올 수 있게 함
 app.use(bodyParser.urlencoded({extended: true})); //application/x-www-from-urlencoded 이렇게 된 데이터를 분석해서 가져올 수 있게 함
@@ -24,7 +23,7 @@ app.get('/', (req, res) => { //루트 디렉토리에 오면
 })
 
 
-app.post('/register', (req,res) => {
+app.post('/api/users/register', (req,res) => {
     //회원 가입 할때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
 
@@ -39,7 +38,7 @@ app.post('/register', (req,res) => {
         })
 })
 
-app.post('/login', (req, res)=>{
+app.post('/api/users/login', (req, res)=>{
 
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({email: req.body.email}, (err, user)=>{
@@ -60,13 +59,31 @@ app.post('/login', (req, res)=>{
           if(err) return res.status(400).send(err) //에러발생을 클라이언트에 전달
 
           //토큰을 저장한다. 어디에? 쿠키, 로컬스토리지 등등
-          res.cookie("X_auth", user.token)
+          res.cookie("x_auth", user.token) //쿠키에 토큰 저장
           .status(200) //성공
           .json({loginSuccess: true, userId: user._id})
       })
     })
   })
 })
+
+// role 1 어드민  role 2 특정 부서 어드민
+// role 0 -> 일반유저  role 0이 아니면 -> 관리자
+app.get('/api/users/auth', auth, (req, res)=> {
+  //여기까지 왔다는 건 Authentication이 True라서 미들웨어를  통과했다는 뜻
+
+  res.status(200).json({ //유저 정보 저장
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true, //일반 유저(0)인지 관리자(!0)인지
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
 
 app.listen(port, () => { //앱이 port에서 listen하면 
   console.log(`Example app listening on port ${port}!`) //콘솔이 출력됨 
--- models/User.js
+++ models/User.js
@@ -72,12 +72,28 @@ userSchema.methods.generateToken = function(cb){
     //jsonwebtoken을 이용해서 token을 생성하기
 
     var token = jwt.sign(user._id.toHexString(), 'secretToken') 
-    //user._id + 'secretToken' = token 이 됨. 나중에 'secretToken'을 통해 user._id 를 알아낼 수 있음
+    //user._id + 'secretToken' = token 이 됨. 나중에 'secretToken'을 통해 token을 decode 해서 user._id 를 알아낼 수 있음
 
     user.token = token
     user.save(function(err, user){
         if(err) return cb(err);
         cb(null, user);
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb){ //토큰을 복호화한 후 유저를 찾는다
+    var user = this;
+
+    //토큰을 decode한다
+    jwt.verify(token, 'secretToken', function(err, decoded){
+        //decoded 는 user._id 를 의미함
+        //유저 아이디를 이용해서 유저를 찾은 다음에
+        //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+
+        user.findOne({"_id": decoded, "token": token}, function(err, user){
+            if(err) return cb(err);
+            cb(null, user)
+        })
     })
 }
 
